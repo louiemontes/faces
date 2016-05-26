44a46ee96f22ba06ebc94c5f68a190aec55eb529
louiemontes <louiemontes@gmail.com>
2016-05-26 11:16:51 -0700

    Fixed CSS faces

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ console.log("hello");
 
 var conn = meshblu.createConnection({});
 
+function setMood(mood) {
+  document.getElementById("face").className="face " + mood;
+  document.getElementById("lefteye").className="eyes lefteye " + mood;
+  document.getElementById("righteye").className="eyes righteye " + mood;
+  document.getElementById("mouth").className="mouth " + mood;
+
+};
+
 conn.on('ready', function(data){
   console.log("ready", data);
   conn.subscribe({ uuid: "c8397dbe-aa11-463a-832b-4c6e5d81bbfc", types: ['broadcast']}, function(err){
@@ -12,4 +20,5 @@ conn.on('ready', function(data){
 
 conn.on('message', function(data){
   console.log('message', data);
+  setMood(data.payload);
 });
